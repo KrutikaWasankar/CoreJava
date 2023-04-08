6dbf80f9eda6265ea9ece8e3738ab1873936eba7
SHREE <SHREE@DESKTOP-AS2C3AC>
2023-04-08 19:28:26 +0530

    ok

---
+++ BasicAssignment/src/com/assignment/NumberDays.java
@@ -0,0 +1,21 @@
+//1. Write a Java program print total number of days in a month using switch case 
+//input month number 1-->january=31 days
+package com.assignment;
+
+import java.util.Scanner;
+
+public class NumberDays {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		System.out.println("Ennter the month");
+		int month = s.nextInt();
+
+		switch (month) {
+		case 1:
+			System.out.println("jan=31 Days");
+		}
+	}
+
+}
