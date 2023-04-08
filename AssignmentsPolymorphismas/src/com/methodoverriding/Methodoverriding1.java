747ec42987ec19caed4ad1192020fce89dd97fda
SHREE <SHREE@DESKTOP-AS2C3AC>
2023-04-08 19:26:17 +0530

    ok

---
+++ AssignmentsPolymorphismas/src/com/methodoverriding/Methodoverriding1.java
@@ -0,0 +1,20 @@
+package com.methodoverriding;
+
+class vehicle {
+	void run() {
+		System.out.println("Vehical is runing");
+	}
+}
+
+class Bike {
+	void run() {
+		System.out.println("Bike is running safetly");
+	}
+}
+
+public class Methodoverriding1 {
+
+	public static void main(String[] args) {
+
+	}
+}
